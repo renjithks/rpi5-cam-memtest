1b2d810bff0bcbdc32dff1d6a857c94315bd1d3d
renjithks <renjith.rohin@gmail.com>
2025-06-22 16:38:13 +0900

    Migrate to libcamera-based capture with DMABUF and OpenCV visualization

--- include/FrameProcessor.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <opencv2/opencv.hpp>
-
-class FrameProcessor {
-public:
-    void process(const cv::Mat& input, cv::Mat& output);
-};
--- src/FrameProcessor.cpp
+++
@@ -1,5 +0,0 @@
-#include "FrameProcessor.hpp"
-
-void FrameProcessor::process(const cv::Mat& input, cv::Mat& output) {
-    cv::cvtColor(input, output, cv::COLOR_BGR2GRAY);
-}
